08c2bc482b53a1dafc301381264682d0bab5356f
Teepan <paartheepan.raveenthiran@radiofrance.com>
2025-03-19 10:05:31 +0100

    feat: add trackApi & codegen

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { ApolloServer } from "@apollo/server";
 import {startStandaloneServer} from '@apollo/server/standalone'
 import { typeDefs } from "./schema.js";
 import { resolvers } from "./resolvers.js";
+import { TrackAPI } from "./datasources/TrackAPI.js";
+import { DataSourceContext } from "./context.js";
 
 const server = new ApolloServer({
   typeDefs,
@@ -10,6 +12,14 @@ const server = new ApolloServer({
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async () => {
+    const cache = server.cache
+    return {
+      dataSources: {
+        trackApi: new TrackAPI({cache})
+      }
+    } satisfies DataSourceContext
+  }
 });
  
 console.log(`🚀  Server ready at: ${url}`);
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,9 @@
 import { GraphQLError } from "graphql"
 import { doctorsData } from "./datasources/doctors.js"
 import { getClosestColor } from "./domain/colors/closestColor.js"
+import { Resolvers } from "./types.js"
 
-export const resolvers = {
+export const resolvers: Resolvers = {
   Query: {
     doctors: (_, args) => args?.specialities ? doctorsData.filter(el => args?.specialities.includes(el.speciality)): doctorsData,
     doctor: (_, {id}) => {
@@ -19,6 +20,14 @@ export const resolvers = {
       if (!(hexa.match(/^#[0-9a-fA-F]{6}/))) throw new GraphQLError('Invalid color provided')
 
       return getClosestColor(hexa, ['#ffffff', '#000000'])
+    },
+    getTracks: async (_, __, {dataSources}) => {
+      const data = await dataSources.trackApi.getTracks()
+      console.log({data})
+      return data
     }
+  },
+  Track: {
+    author: (parent, _, {dataSources}) => dataSources.trackApi.getAuthorBy(parent.authorId)
   }
 }
